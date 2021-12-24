f363110d20c05b298452351cee6d6eebdd43a39a
eugeneKl95 <96632533+eugeneKl95@users.noreply.github.com>
2021-12-24 21:43:18 +0200

    Add files via upload

---
+++ Task4/Task4/Task4/GameHMAC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Task4
+{
+    class GameHMAC
+    {
+        public static byte[] HashHMAC(byte[] key,byte[] message)
+        {            
+            var hash = new HMACSHA256(key);//SHA2
+            return hash.ComputeHash(message);
+        }
+        
+    }
+}
